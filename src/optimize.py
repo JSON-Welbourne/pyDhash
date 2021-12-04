03f69f3211ad86372caa66c90e119443112eb759
Jason Welbourne <95187429+JSON-Welbourne@users.noreply.github.com>
2021-12-04 18:53:55 -0500

    Update optimize.py

--- src/optimize.py
+++ src/optimize.py
@@ -21,57 +21,72 @@ def main(**kwargs):
     tabString = kwargs.get('tabString')
     images = {}
     for p in paths:
-        if os.path.isfile(p):
-            print("Found `{}`".format(p))
-            images[p] = Image.open(p)
-        else:
-            print("Unable to find `{}`".format(p))
+        try:
+            if os.path.isfile(p):
+                print("Found `{}`".format(p))
+                images[p] = Image.open(p)
+            else:
+                print("Unable to find `{}`".format(p))
+        except Exception as e:
+            print("ERROR Initializing Paths, path = {}: {}".format(p,e))
     hashes = {}
     deviations = {}
     for i in range(minI,maxI):
-        for path in paths:
-            key = "{}:{}".format(i,path)
-            hashes[key] = dhash.hashImage('{}{}'.format(hashType,i),images[path])
-            hashString = dhash.hashToString(hashes[key])
-            print("[{}.{}]".format(
-                i, path))
-            if len(hashString) < (76*20):
-                lines = int((len(hashString)/hashChannels) ** (1/2))
-                [print("    {}".format(
-                    " ".join([
-                        hashString[(hashPart * int(len(hashString)/hashChannels)) + (hashLine * int(len(hashString)/(hashChannels*lines))) :
-                                   (hashPart * int(len(hashString)/hashChannels)) + ((hashLine + 1) * int(len(hashString)/(hashChannels*lines)))]
-                        for hashPart in range(5)])))
-                    for hashLine in range(lines)]
-            else:
-                pass
-                # [print("    {}".format(
-                #     hashString[i2 * int(len(hashString)/5): (i2 + 1) * int(len(hashString)/5)])) for i2 in range(5)]
-            if 'errors' in hashes[key] and len(hashes[key]['errors']) > 0:
-                [print("{}{}".format(tabString * 1, e)) for e in hashes[key]['errors'] if e['location'][-1] != 'A']
-        if dhash.hashToString(hashes["{}:{}".format(i,paths[0])]) != dhash.hashToString(hashes["{}:{}".format(i,paths[1])]):
-            deviations[i] = jellyfish.hamming_distance(
-                dhash.decodeHash(dhash.hashToString(hashes["{}:{}".format(i,paths[0])]), parts=hashChannels),
-                dhash.decodeHash(dhash.hashToString(hashes["{}:{}".format(i,paths[1])]), parts=hashChannels) )
-        print("{}Deviation: {} / {} = {}".format(
-            tabString * 1,
-            deviations[i],
-            hashChannels * 2 * i ** 2,
-            int(10000 * deviations[i] / (hashChannels * 2 * i ** 2))/100))
-    # deviations = {
+        try:            
+            for path in paths:
+                try:
+                    key = "{}:{}".format(i,path)
+                    hashes[key] = dhash.hashImage('{}{}'.format(hashType,i),images[path])
+                    hashString = dhash.hashToString(hashes[key])
+                    print("[{}.{}]".format(
+                        i, path))
+                    if len(hashString) < (76*20):
+                        lines = int((len(hashString)/hashChannels) ** (1/2))
+                        [print("    {}".format(
+                            " ".join([
+                                hashString[(hashPart * int(len(hashString)/hashChannels)) + (hashLine * int(len(hashString)/(hashChannels*lines))) :
+                                           (hashPart * int(len(hashString)/hashChannels)) + ((hashLine + 1) * int(len(hashString)/(hashChannels*lines)))]
+                                for hashPart in range(5)])))
+                            for hashLine in range(lines)]
+                    else:
+                        pass
+                        # [print("    {}".format(
+                        #     hashString[i2 * int(len(hashString)/5): (i2 + 1) * int(len(hashString)/5)])) for i2 in range(5)]
+                    if 'errors' in hashes[key] and len(hashes[key]['errors']) > 0:
+                        [print("{}{}".format(tabString * 1, e)) for e in hashes[key]['errors'] if e['location'][-1] != 'A']
+                except Exception as e:
+                    print("ERROR, path = {}: {}".format(path,e))
+            if dhash.hashToString(hashes["{}:{}".format(i,paths[0])]) != dhash.hashToString(hashes["{}:{}".format(i,paths[1])]):
+                deviations[i] = jellyfish.hamming_distance(
+                    dhash.decodeHash(dhash.hashToString(hashes["{}:{}".format(i,paths[0])]), parts=hashChannels),
+                    dhash.decodeHash(dhash.hashToString(hashes["{}:{}".format(i,paths[1])]), parts=hashChannels) )
+            print("{}Deviation: {} / {} = {}".format(
+                tabString * 1,
+                deviations[i],
+                hashChannels * 2 * i ** 2,
+                int(10000 * deviations[i] / (hashChannels * 2 * i ** 2))/100))
+        except Exception as e:
+            print("ERROR, i = {}: {}".format(i,e))
+
+            # deviations = {
     #     k:v for k,v in deviations.items() 
     #     if ((k == minI
     #         or (int(10000 * (v / (5 * 2 * k ** 2))) >= int(10000 * (deviations[max([k2 for k2 in deviations.keys() if k2 < k])] / (5 * 2 * (max([k2 for k2 in deviations.keys() if k2 < k])) ** 2))))
     #     and (k >= maxI 
     #         or int(10000 * (v / (5 * 2 * k ** 2))) >= int(10000 * (deviations[min([k2 for k2 in deviations.keys() if k2 > k])] / (5 * 2 * (min([k2 for k2 in deviations.keys() if k2 > k])) ** 2))))))}
+
     for k,v in deviations.items():
-        print("{}{}: {}{} / {}{} = {}".format(
-            tabString * 1,
-            k,
-            " " * (max([len(str(k1)) + len(str(v1)) for k1,v1 in deviations.items()]) - len(str(k)) - len(str(v))),
-            v, hashChannels * 2 * k ** 2,
-            " " * (max([len(str(hashChannels*2*k1**2)) for k1 in deviations.keys() ]) - len(str(hashChannels*2*k**2))),
-            int(10000 * (v / (hashChannels * 2 * k ** 2)))/100 ))
+        try:
+            print("{}{}: {}{} / {}{} = {}".format(
+                tabString * 1,
+                k,
+                " " * (max([len(str(k1)) + len(str(v1)) for k1,v1 in deviations.items()]) - len(str(k)) - len(str(v))),
+                v, hashChannels * 2 * k ** 2,
+                " " * (max([len(str(hashChannels*2*k1**2)) for k1 in deviations.keys() ]) - len(str(hashChannels*2*k**2))),
+                int(10000 * (v / (hashChannels * 2 * k ** 2)))/100 ))
+        except Exception as e:
+            print("ERROR, (k,v) = {},{}: {}".format(k,v,e))
+
     print("Summary of Hash `{}`".format(hashType))
     deviations = {
         k:v for k,v in deviations.items() 
